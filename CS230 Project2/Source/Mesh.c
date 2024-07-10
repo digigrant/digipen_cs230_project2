901e2543877dca2268b7ffdd6d17e68bafe2e8ad
digigrant <digigrant@users.noreply.github.com>
2024-07-10 00:59:42 -0700

    Implemented Mesh.c

--- CS230 Project2/Source/Mesh.c
+++ CS230 Project2/Source/Mesh.c
@@ -11,6 +11,7 @@
 
 #include "stdafx.h"
 #include "Mesh.h"
+#include "DGL.h"
 
 //------------------------------------------------------------------------------
 // Private Constants:
@@ -19,6 +20,12 @@
 //------------------------------------------------------------------------------
 // Private Structures:
 //------------------------------------------------------------------------------
+typedef struct Mesh
+{
+	char name[32];
+	DGL_Mesh* mesh_resource;
+	DGL_DrawMode draw_mode;
+} Mesh;
 
 //------------------------------------------------------------------------------
 // Public Variables:
@@ -35,6 +42,45 @@
 //------------------------------------------------------------------------------
 // Public Functions:
 //------------------------------------------------------------------------------
+Mesh* MeshCreate()
+{
+	// returns a pointer to a new Mesh object with all fields initialized to 0
+	// returns NULL if memory allocation fails
+	return (Mesh*)calloc(1, sizeof(Mesh));
+}
+
+void MeshFree(Mesh** mesh)
+{
+	DGL_Graphics_FreeMesh(&((*mesh)->mesh_resource));
+	free(*mesh);
+	*mesh = NULL;
+}
+
+void MeshBuildQuad(Mesh* mesh, float xHalfSize, float yHalfSize, float uSize, float vSize, const char* name)
+{
+	// Name
+	strcpy_s(mesh->name, _countof(mesh->name), name);
+
+	// Mesh
+	DGL_Graphics_StartMesh();
+	DGL_Graphics_AddTriangle(
+		&(DGL_Vec2) { -xHalfSize, -yHalfSize }, &(DGL_Color) { 0.0f, 0.0f, 0.0f, 1.0f }, &(DGL_Vec2) { 0.0f, vSize },
+		&(DGL_Vec2) {  xHalfSize,  yHalfSize }, &(DGL_Color) { 0.0f, 0.0f, 0.0f, 1.0f }, &(DGL_Vec2) { uSize, 0.0f },
+		&(DGL_Vec2) {  xHalfSize, -yHalfSize }, &(DGL_Color) { 0.0f, 0.0f, 0.0f, 1.0f }, &(DGL_Vec2) { uSize, vSize });
+	DGL_Graphics_AddTriangle(
+		&(DGL_Vec2) { -xHalfSize, -yHalfSize }, &(DGL_Color) { 0.0f, 0.0f, 0.0f, 1.0f }, &(DGL_Vec2) { 0.0f, vSize },
+		&(DGL_Vec2) { -xHalfSize,  yHalfSize }, &(DGL_Color) { 0.0f, 0.0f, 0.0f, 1.0f }, &(DGL_Vec2) { 0.0f, 0.0f },
+		&(DGL_Vec2) {  xHalfSize,  yHalfSize }, &(DGL_Color) { 0.0f, 0.0f, 0.0f, 1.0f }, &(DGL_Vec2) { uSize, 0.0f });
+	mesh->mesh_resource = DGL_Graphics_EndMesh();
+
+	// Draw Mode
+	mesh->draw_mode = DGL_DM_TRIANGLELIST;
+}
+
+void MeshRender(const Mesh* mesh)
+{
+	DGL_Graphics_DrawMesh(mesh->mesh_resource, mesh->draw_mode);
+}
 
 //------------------------------------------------------------------------------
 // Private Functions:
